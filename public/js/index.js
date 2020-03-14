41b47efc60eaa50cd9417cf44a3eb65de44e83aa
JigyasuPrakash <jigyasuprakash3@gmail.com>
2020-03-15 00:31:55 +0530

    theme imported

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+$.getJSON('https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_time_series?ref=master', function (response) {
+    //Confirmed cases, deaths and recovery data    
+    console.log(response)
+});
+
+$.getJSON('https://api.github.com/repos/CSSEGISandData/COVID-19/contents/csse_covid_19_data/csse_covid_19_daily_reports?ref=master', function (response) {
+    // Daily reports
+    console.log(response)
+});
